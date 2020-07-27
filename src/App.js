44ae70d91ab5ec744da50d01497da7a6970ce360
Nattapon <nattapon.n4p@gmail.com>
2020-07-27 14:43:20 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import bg from './assets/images/bg.jpg';
+import logo from './assets/images/logo.png';
 
 const App = () => {
-  return <div className="app">Good Luck</div>;
+  return<img src={bg} style={{flex:1, 
+      width: "100%" ,
+      height:"100%",
+      resizeMode: 'contain'}}></img>
+
 };
 
 export default App;
